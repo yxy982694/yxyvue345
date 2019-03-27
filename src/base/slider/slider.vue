4dc4be1dbf29cc6bf5a5b9a6382a33cf1d729fb2
yxy982694 <3216320895@qq.com>
2019-03-27 10:04:00 +0800

    歌单页面3.27

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,37 @@
+<template>
+  <swiper :options="swiperOption">
+    <slot></slot>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+<script>
+export default {
+  name: 'slider',
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Number,
+      default: 5000
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        autoplay: this.autoPlay,
+        loop: this.loop,
+        pagination: '.swiper-pagination'
+      }
+    }
+  }
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  >>>.swiper-pagination-bullet-active
+    background: #f00
+    width: 30px
+    height: 8px
+    border-radius: 10px
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>歌手页面</div>
+</template>
+<script type="text/ecmascript-6">
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
