0070a97869a52b26c967499f5c0524eeeb61c614
yxy982694 <3216320895@qq.com>
2019-08-15 14:55:44 +0800

    创建分支  fenzhi815

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 2019年8月15日 fenzhi1添加内容
 import Vue from 'vue'
 import App from './App'
 import createRouter from './router'
